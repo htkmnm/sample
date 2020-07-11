6074fbda6f7e74a9ed1e4c4a11a5abf1eb3360fc
はつき みなみ <hatsukiminami@hatsuki-no-MacBook-Air.local>
2020-07-12 02:00:49 +0900

    NORMAL-1

--- src/App.js
+++ src/App.js
@@ -6,11 +6,16 @@ function App() {
 
   const increment = () => {
     setCount(count + 1);
+  }
 
-  };
+  const decrement = () => {
+    setCount(count - 1);
+  }
+
   return (
     <div>
-      <button onClick={increment}>いいね</button>
+      <button onClick={increment}>いいね！</button>
+      <button onClick={decrement}>よくないね！</button>
       {count}
     </div>
   );
