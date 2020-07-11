aa26a3b52ba3f69cab1036bfd6432f2bec44e84c
はつき みなみ <hatsukiminami@hatsuki-no-MacBook-Air.local>
2020-07-11 22:05:28 +0900

    sample

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-      
-        <p>
-          <button onClick={()=>{window.alert("Hello World")}}>Hello World</button>
-        </p>
-      
-      </header>
+    <div>
+      <button onClick={increment}>いいね</button>
+      {count}
     </div>
   );
 }
