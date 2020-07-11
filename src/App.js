ca32fcb121e0fd2cf941bfabaa2eaa33f4438178
はつき みなみ <hatsukiminami@hatsuki-no-MacBook-Air.local>
2020-07-12 02:29:21 +0900

    EXTRA-2

--- src/App.js
+++ src/App.js
@@ -2,27 +2,16 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState("hatsuki");
 
   const increment = () => {
-    setCount(count + 1);
+    setCount(count + "hatsuki");
     console.log(count);
-  }
-
-  const decrement = () => {
-    setCount(count - 1);
-    console.log(count);
-  }
-
-  const reset = () => {
-    setCount(0);
   }
 
   return (
     <div>
-      <button onClick={increment}>いいね！</button>
-      <button onClick={decrement}>よくないね！</button>
-      <button onClick={reset}>リセット</button>
+      <button onClick={increment}>add</button>
       {count}
     </div>
   );
